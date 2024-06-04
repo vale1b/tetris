f2a5c4df1a775f394f02bfcc5a6f1ef13c7648e0
vale1b <barberisvalentin85@gmail.com>
2024-06-04 16:27:24 -0300

    Finalizando el juego.

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ import './style.css'
 // 1. Inicializar el canvas.
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
+const $score = document.querySelector('span')
 
 const BLOCK_SIZE = 20
 const BOARD_WIDTH = 14
 const BOARD_HEIGHT = 30
+
+let score = 0
 
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
@@ -56,8 +59,38 @@ const piece = {
   ]
 }
 
-// 2. Game loop
-function update() {
+// 9. Piezas Randoms
+const PIECES = [
+  [[1, 1, 1, 1]],
+  [[0, 1, 0],
+   [1, 1, 1]],
+  [[1, 1, 0],
+   [0, 1, 1]],
+  [[1, 0],
+   [1, 0],
+   [1, 1]]
+];
+
+// 8. Auto drop
+let dropCounter = 0
+let lastTime = 0
+
+function update(time = 0) {
+  const deltaTime = time - lastTime
+  lastTime = time
+
+  dropCounter += deltaTime
+
+  if (dropCounter > 1000) {
+    piece.position.y++
+    dropCounter = 0
+
+    if (checkCollision()) {
+      piece.position.y--
+      solidifyPiece()
+      removeRows()
+    }
+  }
   draw()
   window.requestAnimationFrame(update)
 }
@@ -83,8 +116,11 @@ function draw() {
       }
     })
   })
+
+  $score.innerText = score
 }
 
+// Seteamos el movimiento de la pieza.
 document.addEventListener('keydown', event => {
   if (event.key === 'ArrowLeft') {
     piece.position.x--
@@ -103,10 +139,34 @@ document.addEventListener('keydown', event => {
     if (checkCollision()) {
       piece.position.y--
       solidifyPiece()
+      removeRows()
+    }
+  }
+
+  // Rotar piezas.
+  if (event.key === 'ArrowUp') {
+    const rotated = []
+
+    for (let i = 0; i < piece.shape[0].length; i++){
+      const row = []
+
+      for (let j = piece.shape.length - 1; j >= 0; j--) {
+        row.push(piece.shape[j][i])
+      }
+
+      rotated.push(row)
+    }
+    
+    // Chequear la forma de la pieza antes de rotar.
+    const previousShape = piece.shape
+    piece.shape = rotated
+    if (checkCollision()) {
+      piece.shape = previousShape
     }
   }
 })
 
+// Esta funcion sirve para que la pieza del jugador no salga del tablero al tocar los bordes.
 function checkCollision () {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
@@ -118,17 +178,42 @@ function checkCollision () {
   })
 }
 
+// Esta funcion sirve para que la pieza del jugador quede solidificada en la posicion del tablero.
 function solidifyPiece () {
-  piece.shape.forEach((row, x) => {
-    row.forEach((value, y) => {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
       if (value === 1) {
         board[y + piece.position.y][x + piece.position.x] = 1
       }
     })
   })
 
-  piece.position.x = 0
+  // Reseteamos la posicion de la pieza.
+  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2)
   piece.position.y = 0
+  // Que caiga la pieza random.
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+  // Game Over
+  if (checkCollision()) {
+    window.alert('Game Over!! Sorry!')
+    board.forEach((row) => row.fill(0))
+  }
 }
 
+function removeRows () {
+  const rowsToRemove = []
+
+  board.forEach((row, y) => {
+    if (row.every(value => value === 1)){
+      rowsToRemove.push(y)
+    }
+  })
+
+  rowsToRemove.forEach(y => {
+    board.splice(y, 1)
+    const newRow = Array(BOARD_WIDTH).fill(0)
+    board.unshift(newRow)
+    score += 10
+  })
+}
 update()
